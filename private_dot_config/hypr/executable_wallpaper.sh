4409a097a3119e2fa430df8f61f69414c25e7257
mintoleda <fascinatingflo@gmail.com>
2025-12-30 18:19:11 -0600

    waybar colors updated

--- private_dot_config/hypr/executable_wallpaper.sh
+++ private_dot_config/hypr/executable_wallpaper.sh
@@ -10,7 +10,7 @@ main() {
     swww img "$selected_wallpaper" --transition-type any --transition-fps 60 --transition-duration .5
     wal -i "$selected_wallpaper" -n --cols16
     swaync-client --reload-css
-    cat ~/.cache/wal/colors-kitty.conf > ~/.config/kitty/current-theme.conf
+    cat ~/.cache/wal/colors-kitty.conf >~/.config/kitty/current-theme.conf
     pywalfox update
     color1=$(awk 'match($0, /color2=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh)
     color2=$(awk 'match($0, /color3=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh)
@@ -18,7 +18,6 @@ main() {
     sed -i "s/^gradient_color_1 = .*/gradient_color_1 = '$color1'/" $cava_config
     sed -i "s/^gradient_color_2 = .*/gradient_color_2 = '$color2'/" $cava_config
     pkill -USR2 cava 2>/dev/null
-    source ~/.cache/wal/colors.sh && cp -r $wallpaper ~/wallpapers/pywallpaper.jpg 
+    source ~/.cache/wal/colors.sh && cp -r $wallpaper ~/wallpapers/pywallpaper.jpg
 }
 main
-
