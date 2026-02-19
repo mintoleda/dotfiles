d6e687b6cebe0e80e27294f23f788329738b5c1b
mintoleda <fascinatingflo@gmail.com>
2026-02-19 02:25:30 -0600

    chore: updates

--- private_dot_config/quickshell/lib/executable_toggle-eco.sh
+++ private_dot_config/quickshell/lib/executable_toggle-eco.sh
@@ -2,8 +2,8 @@
 
 # toggle-eco.sh
 # Usage: ./toggle-eco.sh [on|off]
-# on:  Switches to Quiet profile, disables animations/blur/shadows
-# off: Switches to Balanced profile, enables animations/blur/shadows
+# on:  Switches to Quiet profile, disables animations/shadows
+# off: Switches to Balanced profile, enables animations/shadows
 
 MODE=$1
 
@@ -16,12 +16,10 @@ if [ "$MODE" == "on" ]; then
     # Enable Eco Mode
     asusctl profile set Quiet
     hyprctl keyword animations:enabled 0
-    hyprctl keyword decoration:blur:enabled 0
     hyprctl keyword decoration:shadow:enabled 0
 elif [ "$MODE" == "off" ]; then
     # Disable Eco Mode (Restore Standard)
     asusctl profile set Balanced
     hyprctl keyword animations:enabled 1
-    hyprctl keyword decoration:blur:enabled 1
     hyprctl keyword decoration:shadow:enabled 1
 fi
